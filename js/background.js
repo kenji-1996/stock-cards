e9ef9e221c3c30350a472453af1d9d453ff2e372
Callum Chisholm <callum@techgorilla.io>
2018-06-10 19:07:34 +1000

    First push

--- js/background.js
+++ js/background.js
@@ -2,6 +2,10 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
         console.log("The color is green.");
     });
+    chrome.fileSystem.getVolumeList(function (e) {
+        console.log(e);
+    });
+
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,9 @@
+// Color changing interaction
 let changeColor = document.getElementById('changeColor');
-
 chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
 });
-
 changeColor.onclick = function(element) {
     let color = element.target.value;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -12,4 +11,9 @@ changeColor.onclick = function(element) {
             tabs[0].id,
             {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
+};
+//Interacting with database
+let queryDB = document.getElementById('queryDatabase');
+queryDB.onclick = function(element) {
+
 };
