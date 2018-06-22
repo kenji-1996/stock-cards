2594504018f46d78ae8ccd934cd11db8e9e36bd4
Callum Chisholm <callum@techgorilla.io>
2018-06-23 08:46:31 +1000

    Working license check

--- js/background.js
+++ js/background.js
@@ -1,34 +1,48 @@
 var server = 'http://10.30.43.107:49691';
 var licence = 'techgorilla-123';
+var active = false;
 chrome.storage.sync.get(function (obj) {
     if(obj['server'] && obj['server'] !== null) {
         server = obj['server'];
         console.log('server',server);
     }
+    if(obj['active'] && obj['active'] !== null) {
+        active = obj['active'];
+        console.log('active',active);
+    }
     if(obj['licence'] && obj['licence'] !== null) {
         licence = obj['licence'];
     }
+    licenseCheck();
 });
-
 chrome.runtime.onInstalled.addListener(function() {
-    //run license check
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        //console.log("The color is green.");
-    });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'my.api.net.au'},
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
+            chrome.declarativeContent.onPageChanged.addRules([{
+                conditions: [new chrome.declarativeContent.PageStateMatcher({pageUrl: {hostEquals: 'my.api.net.au'},})],
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+            }]);
     });
 });
 
-chrome.runtime.onStartup.addListener(function() {
-    alert('test');
-});
+function licenseCheck() {
+    if(jQuery) {
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "https://billing.techgorilla.io/modules/servers/licensing/check.php?license=" + licence,
+            success: function (data) {
+                if (data.status === 'Active') {
+                    chrome.storage.sync.set({active: true}, function () {console.log('active set to true')});
+                } else {
+                    chrome.storage.sync.set({active: false}, function () { chrome.storage.sync.set({active: true}, function () {console.log('active set to false')});});
+                }
+            }
+        }).done(function () {
+        });
+    }else{
+        alert('License check failed due to jQuery');
+    }
+}
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
