a047f3b055e0266e964e8d5ec908ea864fc07caf
Callum <crowhf@y7mail.com>
2018-07-29 12:15:51 +1000

    Fix modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,41 +71,49 @@ function loadScript() {
                 if (response) {
                     console.log(response.result.data[0]);
                     product.element.append("" +
-                        "<div class='modal fade' id='" + response.result.data[0].StockID + "' tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">" +
-                        "  <div class=\"modal-dialog modal-lg\">" +
-                        "   <button type=\"button\" class='btn btn-warning' data-dismiss=\"modal\" aria-label=\"Close\">Close</button>" +
+                        "<div id=" + response.result.data[0].StockID +"  class=\"modal fade\" id=\"myModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\">" +
+                        "  <div class=\"modal-dialog\" role=\"document\">" +
                         "    <div class=\"modal-content\">" +
+                        "       <div class=\"modal-header\">" +
+                        "           <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>" +
+                        "           <h4 class=\"modal-title\" id=\"myModalLabel\">" +response.result.data[0].TradeName + "</h4>" +
+                        "       </div>" +
+                        "      <div class=\"modal-body\">" +
                         "<table class='table'>" +
-                        "<thead><tr>" +
-                        "<th scope='col'>StockID</th>" +
-                        "<th scope='col'>TradeName</th>" +
-                        "<th scope='col'>SOH</th>" +
-                        "<th scope='col'>MinimumSOH</th>" +
-                        "<th scope='col'>Reorder</th>" +
-                        "<th scope='col'>PLU</th>" +
-                        "<th scope='col'>LastOrderDate</th>" +
-                        "<th scope='col'>UPI</th>" +
-                        "<th scope='col'>MinOrdQty</th>" +
-                        "<th scope='col'>MaximumSOH</th>" +
-                        "</tr></thead>" +
-                        "<tbody><tr>" +
-                        "<td>" + response.result.data[0].StockID + "</td>" +
-                        "<td>" + response.result.data[0].TradeName + "</td>" +
-                        "<td>" + response.result.data[0].SOH + "</td>" +
-                        "<td>" + response.result.data[0].MinimumSOH + "</td>" +
-                        "<td>" + response.result.data[0].Reorder + "</td>" +
-                        "<td>" + response.result.data[0].PLU + "</td>" +
-                        "<td>" + response.result.data[0].LastOrderDate + "</td>" +
-                        "<td>" + response.result.data[0].UPI + "</td>" +
-                        "<td>" + response.result.data[0].MinOrdQty + "</td>" +
-                        "<td>" + response.result.data[0].MaximumSOH + "</td>" +
-                        "</tr></tbody>" +
+                            "<thead><tr>" +
+                            "<th scope='col'>StockID</th>" +
+                            "<th scope='col'>SOH</th>" +
+                            "<th scope='col'>MinimumSOH</th>" +
+                            "<th scope='col'>Reorder</th>" +
+                            "<th scope='col'>PLU</th>" +
+                            "<th scope='col'>LastOrderDate</th>" +
+                            "<th scope='col'>UPI</th>" +
+                            "<th scope='col'>MinOrdQty</th>" +
+                            "<th scope='col'>MaximumSOH</th>" +
+                            "</tr></thead>" +
+                            "<tbody><tr>" +
+                            "<td>" + response.result.data[0].StockID + "</td>" +
+                            "<td>" + response.result.data[0].SOH + "</td>" +
+                            "<td>" + response.result.data[0].MinimumSOH + "</td>" +
+                            "<td>" + response.result.data[0].Reorder + "</td>" +
+                            "<td>" + response.result.data[0].PLU + "</td>" +
+                            "<td>" + response.result.data[0].LastOrderDate + "</td>" +
+                            "<td>" + response.result.data[0].UPI + "</td>" +
+                            "<td>" + response.result.data[0].MinOrdQty + "</td>" +
+                            "<td>" + response.result.data[0].MaximumSOH + "</td>" +
+                            "</tr></tbody>" +
                         "</table>" +
+                        "      </div>" +
+                        "      <div class=\"modal-footer\">" +
+                        "        <button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button>" +
+                        "      </div>" +
+                        "    </div>" +
+                        "  </div>" +
                         "</div>");
                     product.element.find(".product-name a").after(
                         "<br/><span class='badge badge-success small'>" + response.result.data[0].SOH + "</span>" +
                         "<br/><span class='badge badge-warning small'>" + response.result.data[0].TradeName + "</span>" +
-                        "<br/><span class='badge badge-info small' data-toggle='modal' data-target='#" + response.result.data[0].StockID + "'>StockCards Info</span>"
+                        "<br/><span class='badge badge-info small pointer' data-toggle='modal' data-target='#" + response.result.data[0].StockID + "'>StockCards Info</span>"
                     );
                     product.element.find(".product-name").after(
                     );
